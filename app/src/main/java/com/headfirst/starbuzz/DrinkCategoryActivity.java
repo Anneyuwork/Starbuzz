4fe1de4fc0140648411a693a31df16458135a35e
rongyu <anne.yu.work@gmail.com>
2020-12-22 23:30:59 -0800

    pass the drinkId from drinkCategoryActivity to DrinkActivity

--- app/src/main/java/com/headfirst/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/headfirst/starbuzz/DrinkCategoryActivity.java
@@ -2,7 +2,11 @@ package com.headfirst.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,8 +16,24 @@ public class DrinkCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
+
+        //Opt 2: dynamically use ArrayAdapter.setAdapter() and Drink.toString() to display ListView: activity_drink_category
         ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
+
+        //Create the listener
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                //pass the drink the user clicks on to DrinkActivity, DrinkActivity and DrinkCategoryActivity using the same list of data
+                Intent intent = new Intent(DrinkCategoryActivity.this,DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int)id);
+                startActivity(intent);
+            }
+        };
+
+        //Assign the listener to the list view
+        listDrinks.setOnItemClickListener(itemClickListener);
     }
 }
