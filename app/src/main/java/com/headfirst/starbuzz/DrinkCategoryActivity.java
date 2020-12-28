735a2757b8c3447199f6d47d1997d06239e0ad12
rongyu <anne.yu.work@gmail.com>
2020-12-27 16:02:25 -0800

    using SimpleCursorAdapter to change the DrinkCategoryActivity: getting data from database

--- app/src/main/java/com/headfirst/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/headfirst/starbuzz/DrinkCategoryActivity.java
@@ -3,14 +3,23 @@ package com.headfirst.starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private Cursor cursor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +27,26 @@ public class DrinkCategoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drink_category);
 
         //Opt 2: dynamically use ArrayAdapter.setAdapter() and Drink.toString() to display ListView: activity_drink_category
-        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
-        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
-        listDrinks.setAdapter(listAdapter);
+        //ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
 
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK",
+                    new String[]{"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{android.R.id.text1},
+                    0);
+
+            listDrinks.setAdapter(listAdapter);
+        } catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
         //Create the listener
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
@@ -35,5 +60,13 @@ public class DrinkCategoryActivity extends AppCompatActivity {
 
         //Assign the listener to the list view
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    //The cursor will stay open until the cursor adapter no longer needs it
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
